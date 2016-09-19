66fd2e6f5f5016612bbbac99cea03492741bd140
Tom Hunger <tehunger@gmail.com>
2016-09-19 15:57:49 +0100

    Add toggle to disable sending of auth headers.

--- bg.js
+++ bg.js
@@ -3,6 +3,9 @@ var KEYS = ["x-dex-name", "x-dex-email", "x-dex-email-verified"];
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(details) {
+    if (localStorage["anonymous"] === "true") {
+      return details.requestHeaders;
+    }
 
     KEYS.forEach(function(x) {
       var lx = localStorage[x] || "";
--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 var KEYS = ["x-dex-name", "x-dex-email", "x-dex-email-verified"];
 
 document.getElementById("header-details").onsubmit = function() {
+  localStorage["anonymous"] = document.getElementById("anonymous").checked;
   KEYS.forEach(function(x) {
     localStorage[x] = document.getElementById(x).value;
   });
   window.close();
 };
 KEYS.forEach(function(x) {
+  document.getElementById("anonymous").checked = (localStorage["anonymous"] === "true");
   document.getElementById(x).value = localStorage[x] || "";
 });
